7db2d2bf4d2c8f432f4690da529868e2c736881d
simeonhhristov <simeon.hr01@gmail.com>
2022-04-01 22:55:49 +0300

    Improve homework 1 code

--- hw-1/implementation.cpp
+++ hw-1/implementation.cpp
@@ -61,6 +61,8 @@ private:
     int getIdxOfEarliestToCome()
     {
         int idx = 0;
+        // get index of earliest to come client so he can be taken care of first
+        // after a worker delivers products
         for (int i = 1; i < waitingClients.size(); i++)
         {
             if (waitingClients[i].arriveMinute < waitingClients[idx].arriveMinute)
@@ -71,6 +73,7 @@ private:
         return idx;
     }
 
+    // get next event
     int timeOfEarliestEvent()
     {
         int a, b, c;
@@ -95,6 +98,7 @@ private:
 
     void clientDepart(int idx, int bananas, int schweppes)
     {
+        // determine amount of resources he will take
         int bananasToTake = bananas;
         int schweppesToTake = schweppes;
 
@@ -107,9 +111,11 @@ private:
             schweppesToTake = this->schweppesCount;
         }
 
+        // deduct that amout from the store stock
         this->bananaCount -= bananasToTake;
         this->schweppesCount -= schweppesToTake;
 
+        // for each futureStock deduct that same amount
         for (int i = 0; i < futureStock.size(); i++)
         {
             futureStock[i].bananaCount -= bananasToTake;
@@ -120,19 +126,27 @@ private:
         {
             actionHandler->onClientDepart(idx, this->time, bananasToTake, schweppesToTake);
         }
+
+        // print event
         std::cout << idx << " " << this->time << " " << bananasToTake << " " << schweppesToTake << std::endl;
     }
 
     void clientOrder(ResourceType resource)
     {
+        // When you sent a worker he becomes unavailable to serviceo other clients
         this->workerCount--;
+
         if (resource == ResourceType::banana)
         {
             if (this->actionHandler)
             {
                 actionHandler->onWorkerSend(this->time, ResourceType::banana);
             }
+
+            // add to sentWorkers queue to await return
             sentWorkers.enqueue(Worker(this->time + 60, ResourceType::banana));
+
+            // print current event
             std::cout << "W " << this->time << " banana" << std::endl;
         }
         else
@@ -141,15 +155,22 @@ private:
             {
                 actionHandler->onWorkerSend(this->time, ResourceType::schweppes);
             }
+
+            // add to sentWorkers queue to await return
             sentWorkers.enqueue(Worker(this->time + 60, ResourceType::schweppes));
+
+            // print current event
             std::cout << "W " << this->time << " schweppes" << std::endl;
         }
     }
 
     void workerRestock()
     {
+        // worker has returned and can again service clients
         Worker currentWorker = sentWorkers.dequeue();
         this->workerCount++;
+
+        // resource type he restocked
         if (currentWorker.resource == ResourceType::banana)
         {
             if (this->actionHandler)
@@ -157,6 +178,8 @@ private:
                 actionHandler->onWorkerBack(this->time, ResourceType::banana);
             }
             this->bananaCount += 100;
+
+            // print current event
             std::cout << "D " << this->time << " banana" << std::endl;
         }
         else
@@ -166,6 +189,7 @@ private:
                 actionHandler->onWorkerBack(this->time, ResourceType::schweppes);
             }
             this->schweppesCount += 100;
+            // print current event
             std::cout << "D " << this->time << " schweppes" << std::endl;
         }
 
@@ -201,42 +225,59 @@ private:
     {
         Client currClient = clients.dequeue();
         currClient.maxWaitTime += currClient.arriveMinute;
+        // index used to track the order clients come in
         currClient.arriveMinute = indexOfLastlyArrivedClient++;
 
         if (currClient.bananaCount <= this->bananaCount && currClient.schweppesCount <= this->schweppesCount)
         {
+            // if we have enough resources serve him
             clientDepart(currClient.arriveMinute, currClient.bananaCount, currClient.schweppesCount);
         }
+        // if we need both resources but have only 1 worker available
         else if (currClient.bananaCount > this->bananaCount && currClient.schweppesCount > this->bananaCount && workerCount == 1)
         {
+            // create new FutureStock
             FutureStock fStock = FutureStock(this->bananaCount, this->schweppesCount, this->time + 60);
+
+            // if he is in mor eneed of bananas send a worker for bananas
             if (currClient.bananaCount - this->bananaCount >= currClient.schweppesCount - this->schweppesCount)
             {
                 clientOrder(ResourceType::banana);
                 fStock.bananaCount += 100;
             }
+            // send for schweppes
             else
             {
                 clientOrder(ResourceType::schweppes);
                 fStock.schweppesCount += 100;
             }
+            // add him to the waiting people
             waitingClients.push_back(currClient);
+            // add futureStock to the furureStock tracking vector
             futureStock.push_back(fStock);
         }
+        // if we have more than one workers available
         else if ((currClient.bananaCount > this->bananaCount || currClient.schweppesCount > this->schweppesCount) && this->workerCount >= 1)
         {
+            // create new FutureStock
             FutureStock fStock = FutureStock(this->bananaCount, this->schweppesCount, this->time + 60);
+            
+            //check if there won't be enough stock before he leaves
             if (currClient.bananaCount > this->bananaCount && !willBeDeliveredEnough(currClient.maxWaitTime, currClient.bananaCount, ResourceType::banana))
             {
+                //send a worker
                 clientOrder(ResourceType::banana);
                 fStock.bananaCount += 100;
             }
             if (currClient.schweppesCount > this->schweppesCount && !willBeDeliveredEnough(currClient.maxWaitTime, currClient.schweppesCount, ResourceType::schweppes))
             {
+                //send a worker
                 clientOrder(ResourceType::schweppes);
                 fStock.schweppesCount += 100;
             }
+            //add client to the waiting people
             waitingClients.push_back(currClient);
+            // add futureStock to the furureStock tracking vector
             futureStock.push_back(fStock);
         }
         else
@@ -247,8 +288,8 @@ private:
 
     // 011 - clients and waiting clients
     void case3()
-    {   
-        int idx = getIdxOfnextClientToLeave();
+    {
+        int idx = getIdxOfnextClientToLeave(); 
         if (clients.peek().arriveMinute <= waitingClients[idx].maxWaitTime)
         {
             case2();
@@ -261,7 +302,7 @@ private:
 
     // 100 - only workers to return
     void case4()
-    {   
+    {
         workerRestock();
     }
 
@@ -302,7 +343,8 @@ private:
 
     // 111 - all
     void case7()
-    {
+    {   
+        //order of events: client arrive -> worker return -> client depart
         int idx = getIdxOfnextClientToLeave();
         if (clients.peek().arriveMinute <= sentWorkers.peek().returnTime && clients.peek().arriveMinute <= waitingClients[idx].maxWaitTime)
         {
--- hw-1/interface.h
+++ hw-1/interface.h
@@ -7,15 +7,29 @@ enum class ResourceType
     schweppes,
 };
 
+/// Interface used to gather all events your solution will generate
+/// Instance of this class will be created by the tests and passed to your implementation
 struct ActionHandler
 {
+    /// Callback for when store sends out a worker
+	/// @param minute - the time at which the worker is sent out
+	/// @param resource - the resource the worker will restock
     virtual void onWorkerSend(int minute, ResourceType resource) = 0;
 
+    /// Callback for when a previously sent out worker returns to the store
+	/// @param minute - hte time at which the worker returns to the store
+	/// @param resource - the type of resource the worker restocks
     virtual void onWorkerBack(int minute, ResourceType resource) = 0;
 
+    /// Callback for when client departs from the store for any reason
+	/// @param index - the index of the client (0 - N, where N is the number of clients)
+	/// @param minute - the time at which the client departs from the store
+	/// @param banana - amount of bananas the client takes when departing
+	/// @param schweppes - amount of schweppes the client takes when departing
     virtual void onClientDepart(int index, int minute, int banana, int schweppes) = 0;
 };
 
+/// Expected product stock on next worker's return
 struct FutureStock
 {
     int bananaCount;
@@ -42,6 +56,7 @@ struct Client
                                                                                          maxWaitTime(maxWaitTime_)
     {
     }
+
     friend std::istream &operator>>(std::istream &in, Client &other)
     {
         in >> other.arriveMinute >> other.bananaCount >> other.schweppesCount >> other.maxWaitTime;
@@ -49,6 +64,7 @@ struct Client
     }
 };
 
+/// Descriptor of a worker that makes deliveries at the store
 struct Worker
 {
     int returnTime = 0;
@@ -64,19 +80,40 @@ struct Worker
 
 struct Store
 {
+    /// This method is called to initialize the store state
+	/// For the console executable, starting resources are 0, but some tests will provide amounts more than 0
+	/// @param workerCount - the number of workers this store has
+	/// @param startBanana - the amount of bananas the store starts with
+	/// @param startSchweppes - the amount of schweppes the store starts with
     virtual void init(int workerCount, int startBanana = 0, int startSchweppes = 0) = 0;
 
+    /// This method will be called by the test to set the instance of the ActionHandler
+	/// Your solution *MUST* call the methods of ActionHandler when event happens in the store
+	/// Events in the store are: worker sent out, worker returns, client departs
+	/// NOTE: For the console executable you can choose if you want to use ActionHandler at all
+	/// @param handler - pointer to the instance of the handler, never null
     virtual void setActionHandler(ActionHandler *handler) = 0;
 
+    /// Method used by the tests to add new clients to the store
+	/// Indices of the clients must be incremented starting from 0
+	/// NOTE: Tests may call this method multiple times, this must append new clients to existing clients in the store
     virtual void addClients(const Client *clients, int count) = 0;
 
+    /// Advance the time to the given minute. All events up to and *including* the minute must be generated
+	/// Events that happen at a later time must *not* be generated
+	/// NOTE: Multiple calls to the function will always pass increasing values
+	/// @param minute - the minute to advance the time to
     virtual void advanceTo(int minute) = 0;
 
+    /// Get the amount of bananas currently in the store
     virtual int getBanana() const = 0;
 
+    /// Get the amount of schweppes currently in the store
     virtual int getSchweppes() const = 0;
 
     virtual ~Store() {}
 };
 
+/// This function creates new instance of the store
+/// This will be used by the tests to create a store to test
 Store *createStore();
